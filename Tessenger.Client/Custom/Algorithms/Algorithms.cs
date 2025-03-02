29f2d52d01365deaa301589db76c7e4ba10d7072
Farjaul Islam Rifat <farjaulislamrifat123@outlook.com>
2025-03-02 08:45:04 +0600

    Sunday, March 2, 2025 8:44:55 AM

---
+++ Tessenger.Client/Custom/Algorithms/Algorithms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tessenger.Client.Custom.Algorithms
+{
+    public class Algorithms : IAlgorithms
+    {
+        int IAlgorithms.pro(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tessenger.Client/Custom/Algorithms/IAlgorithms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tessenger.Client.Custom.Algorithms
+{
+    public  interface IAlgorithms
+    {
+        public  int pro(int a, int b);
+        
+    }
+}
